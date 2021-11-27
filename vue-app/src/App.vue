27c945ffcc5d5f8cbd176937451cf3a7169ba0e6
Rezowana Akter <rezowana10@gmail.com>
2021-11-27 23:22:37 +0600

    employee table added

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your First Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-table/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from './components/EmployeeTable.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    EmployeeTable
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button {
+    background: purple;
+    border: 1px solid purple;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
 </style>
